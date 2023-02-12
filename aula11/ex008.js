2163f69cc0f6d4aff1a433c9d3e2db123ec362f6
mandscs <dacostaa641@gmail.com>
2023-02-12 12:53:51 -0300

    aula11pt-1

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 75.5
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60 ) { //condição simples 
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`dirija usando sempre cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'FRANÇA'
+console.log(`vivendo em ${país}`)
+if (país != 'brasil') {
+    console.log('você é estrangeiro')
+} else{
+    console.log('você é brasileiro')
+}
